280427350246c26737dfd348e57ccc571e5010f8
khielcalvinbernardoscc-arch <khielcalvinbernardo.scc@gmail.com>
2025-09-07 18:44:18 +0800

    Add files via upload

---
+++ sw9.java
@@ -0,0 +1,11 @@
+public class sw9 {
+    public static void main(String args[]) {
+int[] numbers = {10, 20, 30, 40, 50};
+int index = 0;
+while(index <5) {
+System.out.print("value of item: " + numbers[index]);
+index++;
+System.out.print("\n");
+}
+}
+}
