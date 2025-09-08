fad99ef0eb01f049da689a7b1bf7006d10dc5e72
khielcalvinbernardoscc-arch <khielcalvinbernardo.scc@gmail.com>
2025-09-08 21:07:04 +0800

    sdadw

---
+++ sw11part2.java
@@ -0,0 +1,15 @@
+public class sw11part2 {
+    public static void main(String[] args) {
+   
+        System.out.println("SEATWORKELEVENPART2");
+
+        int[][] myNumbers = { 
+            {1, 4, 2}, 
+            {3, 6, 8} 
+        };
+
+        // Print specific element
+        System.out.println(myNumbers[1][2]);
+    }
+}
+    
