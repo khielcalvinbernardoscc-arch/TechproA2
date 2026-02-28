04e15fa79c3a64994abdfd959421950dc46dd0c9
khielcalvinbernardoscc-arch <khielcalvinbernardo.scc@gmail.com>
2026-02-28 13:47:59 +0000

    .

---
+++ QUARTER 3 AND 4/Seatworkfive.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Seatwork 5: Espanto Yvhonne Michaella");
+        System.out.println("Countdown");
+        int i = 0;
+        while (i < 10) {
+            System.out.println(i);
+            i++;
+            if (i == 4) {
+                break;
+            }
+        }
+
+        System.out.println("Happy New Birthday ");
+    }
+}
