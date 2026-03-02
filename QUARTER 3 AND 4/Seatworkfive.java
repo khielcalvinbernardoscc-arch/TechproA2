c0c165c293670b244984661ac4cd528f10ab85c5
khielcalvinbernardoscc-arch <khielcalvinbernardo.scc@gmail.com>
2026-03-02 13:45:38 +0800

    Update Seatworkfive.java

--- QUARTER 3 AND 4/Seatworkfive.java
+++ QUARTER 3 AND 4/Seatworkfive.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Seatwork 5: Espanto Yvhonne Michaella");
+        System.out.println("Seatwork 5: Sulasok");
         System.out.println("Countdown");
         int i = 0;
         while (i < 10) {
